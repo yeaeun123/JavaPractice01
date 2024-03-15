7e10ebe5bf816eb58a8bc695943ea60aa279658b
신예은 <yeaeun0125@gmail.com>
2024-03-15 17:06:47 +0900

    JavaPractice01

---
+++ src/com/javapractice/JavaPractice2.java
@@ -0,0 +1,22 @@
+package com.javapractice;
+
+public class JavaPractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a = 5;
+			System.out.println(a); //5 (그대로 출력)
+			System.out.println(++a); //6 (++는 +1추가와 같다)
+			System.out.println(a); //6 (그대로 출력)
+			System.out.println(a++); //6 (++는 1추가)
+			System.out.println(a); //7
+			System.out.println(--a); //6
+			System.out.println(a); //6
+			System.out.println(a--); //6
+			System.out.println(a); //5
+			
+			
+	}
+
+}
