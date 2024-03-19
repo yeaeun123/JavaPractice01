4d589c823f9258941f72eabacaddc9d0c1bacc8e
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-19 16:08:01 +0900

    JavaP

--- src/com/javapractice/Java3_12.java
+++ src/com/javapractice/Java3_12.java
@@ -1,10 +1,25 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class Java3_12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		int num = scanner.nextInt();
+		
+		int fac =1;
+		for (int i =num; i>0; i--) {
+			fac = fac*i;}
+		{System.out.println( "결과값:" +fac);
+		}
 
+		
+		scanner.close();
 	}
-
 }
+
+
---
+++ src/com/javapractice/Java3_13.java
@@ -0,0 +1,27 @@
+package com.javapractice;
+
+import java.util.Scanner;
+
+public class Java3_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요.:");
+		int n = scanner.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i<=n; i++)
+			sum += i;
+		System.out.println("합계:"+ sum);
+		
+		scanner.close();
+		
+
+
+	}
+
+}
