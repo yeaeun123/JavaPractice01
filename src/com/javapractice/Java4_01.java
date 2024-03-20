24982855a3f066aaafe9a699d47dcb2580b8d246
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-20 15:55:14 +0900

    240320오후

---
+++ src/com/javapractice/Java4_01.java
@@ -0,0 +1,17 @@
+//package com.javapractice;
+//
+//public class Java4_01 {
+//
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int[] inArray = new int[5];
+//		intArray[0] = 3;
+//		intArray[1] = 7;
+//		intArray[2] =12;
+//		
+//		int result = 0;
+//		
+//		
+//	}
+//
+//}
