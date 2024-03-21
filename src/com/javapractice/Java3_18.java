6a4fcb3c508f5e9192fbdf916f424f7fbd86aa51
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-21 18:07:28 +0900

    240321

--- src/com/javapractice/Java3_18.java
+++ src/com/javapractice/Java3_18.java
@@ -1,10 +1,25 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class Java3_18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요:");
+		int num = scanner.nextInt();
+		int i,j;
+		for (i=0; i<=num ; i++) {
+			for(j=num-1; j>=i; j--) {
+			System.out.print("*");
+		}
+		System.out.println();
+	
+			}
+	
+		scanner.close();
+	
 	}
 
 }
