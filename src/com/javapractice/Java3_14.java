c97702f4f15f7bedffa42a00e44f0ac8bd47cbaa
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-22 15:25:31 +0900

    240322

--- src/com/javapractice/Java3_14.java
+++ src/com/javapractice/Java3_14.java
@@ -14,8 +14,16 @@ public class Java3_14 {
 		
 		int sum = 0;
 		
-		for(int i = 1; i<=n; i++)
-			sum += i;
+		for(int i = 1; i<=n; i++) {
+			sum +=i;
+			System.out.print(i);
+			if(i < n) {
+				System.out.print("+");
+			}
+			
+		}
+
+			System.out.println();
 		
 		System.out.println("합계:"+ sum);
 		
