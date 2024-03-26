34389a50a1e4d9aa0481d3509e9750f92de968d1
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-26 18:11:48 +0900

    0326

--- src/com/javapractice04/Java4_06.java
+++ src/com/javapractice04/Java4_06.java
@@ -4,6 +4,7 @@ public class Java4_06 {
 
 	public static void main(String[] args) {
 		// [문제] Ex06 주어진 문자열의 공백을 ‘,’(콤마) 로 변경 후 출력하세요.
+		//This is a pencil -> This,is,a,pencil 
 		
 		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
 
