f6bf77f59b6f0d2a2da16e4aab0f4c5f81f6a12b
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-26 16:51:15 +0900

    0326

--- src/com/javapractice/Java3_18.java
+++ src/com/javapractice/Java3_18.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Java3_18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("숫자를 입력하세요:");
 		int num = scanner.nextInt();
